eae5fb374908a31584ea68d0cbf686ca990dd190
Cassidy Murphy <cassidymurphy@Cassidys-MBP.attlocal.net>
2019-09-21 11:39:27 -0400

    Home Page Nav Bar

--- project/app/views.py
+++ project/app/views.py
@@ -27,7 +27,7 @@ def get_response(request):
 
 
 def home(request, template_name="home.html"):
-	context = {'title': 'DayMaker Version 1.0'}
+	context = {'title': 'DayMaker - Home'}
 	return render_to_response(template_name, context)
 
 def chat(request, template_name="chat.html"):
