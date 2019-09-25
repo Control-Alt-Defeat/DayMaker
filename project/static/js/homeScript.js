b83cc1b8801dc9ce05be6796470577c1627a9bd1
Cassidy Murphy <cassidymurphy@Cassidys-MBP.attlocal.net>
2019-09-24 21:42:05 -0400

    Home page populates with events in homeScript.js

--- project/static/js/homeScript.js
+++ project/static/js/homeScript.js
@@ -1,1 +1,20 @@
-
+window.onload = function () {
+    Vue.component('event-item', {
+        props: ['event'],
+        template: '<div> <p> {{ event.location }} </p><p> {{ event.type }} </p><p> {{ event.start }} - {{ event.end }} </p></div>'
+    })
+    
+    var app = new Vue({
+	  delimiters: ['[[', ']]'],
+	  el: '#app',
+	  data: {
+        eventList: [
+            { id: 0, location: 'Cazuelas', type: 'Restaurant', start: '5:00 pm', end: '6:30 pm' },
+            { id: 1, location: 'Newport', type: 'Concert', start: '7:00 pm', end: '11:00 pm' },
+            { id: 2, location: 'Dahlia', type: 'Club', start: '12:00 am', end: '2:00 am'},
+        ],
+        addEventString: '+ Add Event',
+        showBottomEventButton: true,
+      },
+    });
+}
