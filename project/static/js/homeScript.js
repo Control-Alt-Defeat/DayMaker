68943aaf4039a480701852844f6218ba4f4dc70f
Cassidy Murphy <cassidymurphy@Cassidys-MBP.attlocal.net>
2019-09-25 20:03:47 -0400

    Displaying more event info

--- project/static/js/homeScript.js
+++ project/static/js/homeScript.js
@@ -3,11 +3,21 @@ window.onload = function () {
         props: ['event'],
         template: '\
         <div>\
-            <p> {{ event.location }}  \
-                <button v-on:click="$emit(\'remove\')">Remove</button>\
-            </p>\
-            <p> {{ event.type }} </p>\
-            <p> {{ event.start }} - {{ event.end }} </p>\
+            <div class="eventColumnLeft eventDetails"">\
+                <p> {{ event.address }} </p>\
+                <p> {{ event.price }} </p>\
+            </div>\
+            <div class="eventColumnMiddle">\
+                <p class="eventTitle"> {{ event.location }}  \
+                </p>\
+                <p class="eventDetails"> {{ event.type }} </p>\
+                <p class="eventDetails"> {{ event.start }} - {{ event.end }} </p>\
+            </div>\
+            <div class="eventColumnRight">\
+                <p>\
+                    <button v-on:click="$emit(\'remove\')">Remove</button>\
+                </p>\
+            </div>\
         </div>\
         '
     })
@@ -17,9 +27,9 @@ window.onload = function () {
 	  el: '#app',
 	  data: {
         eventList: [
-            { id: 0, location: 'Cazuelas', type: 'Restaurant', start: '5:00 pm', end: '6:30 pm' },
-            { id: 1, location: 'Newport', type: 'Concert', start: '7:00 pm', end: '11:00 pm' },
-            { id: 2, location: 'Dahlia', type: 'Club', start: '12:00 am', end: '2:00 am'},
+            { id: 0, location: 'Cazuelas', type: 'Restaurant', start: '5:00 pm', end: '6:30 pm', address: '123 North High Street', price: '$' },
+            { id: 1, location: 'Newport', type: 'Concert', start: '7:00 pm', end: '11:00 pm', address: '2231 North High Street', price: '$' },
+            { id: 2, location: 'Dahlia', type: 'Club', start: '12:00 am', end: '2:00 am', address: '3000 North High Street', price: '$$'},
         ],
         addEventString: '+ Add Event',
       },
