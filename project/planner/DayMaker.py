c4a5a3dbb1f7a020a7b11c6766f18ae511cadbe2
BrandoCommando37 <brandonarbuthnot7@gmail.com>
2019-11-07 18:03:24 -0500

    Clean DayMaker, create coordinate rules

--- project/planner/DayMaker.py
+++ project/planner/DayMaker.py
@@ -2,23 +2,18 @@
 # Group: Ctrl+Alt+Defeat
 # Application: DayMaker
 
-from . import config
+import config
 import os
 import json
-from . import mvp_event
+import mvp_event
+import dist_func
+import tests_DayMaker
+
+CBUS_COORD = {"latitude": 39.95901, "longitude": -82.99869}
 
 ## Function definitions
 
-# Main function (called when "python DayMaker.py" is run)
-def main():
-
-    # List where (index = hour * 4 + minute/15)
-    dayList = [-1] * 96
-
-    # Function that runs tests on startup
-    ############################ REMOVE LINE COMMENT TO RUN TESTS ON EXECUTION ###################################
-    # runTests(dayList)
-
+# Adds bounds to schedule
 def scheduleBounds(start, finish, dayList):
         start_index = timeConvert(start)
         end_index = timeConvert(finish)
@@ -77,7 +72,13 @@ def createEvent(item, start, end):
     return my_event
 
 # Queries discovery data base using natural language query
-def natLangQuery(queryStr = '', query_filter = '' , num_results=50):
+def natLangQuery(queryStr = '', query_filter = '', num_results=100, distance=100, aCoord=CBUS_COORD):
+
+    if (query_filter == ''):
+        query_filter = coordRule(distance, CBUS_COORD)
+    else:
+        query_filter = andRule(query_filter, coordRule(distance, CBUS_COORD))
+
     my_query = config.discovery.query(config.environment_id,
                             config.collection_id,
                             count=num_results,
@@ -105,15 +106,24 @@ def groupRule(rule):
     new_rule = '(' + rule + ')'
     return new_rule
 
-# def buildQuery(query_filter = '', num_results=50):
-#     my_filter = buildRule('price', "\"$\"")
-#     print(my_filter)
-#     my_query = config.discovery.query(config.environment_id,
-#                             config.collection_id,
-#                             count=num_results,
-#                             filter=query_filter,
-#                             natural_language_query=queryStr)
-#     return json.loads(json.dumps(my_query.result, indent=2))
+def coordRule(distance, aCoord=CBUS_COORD):
+    # Miles -> diff in lat/long
+    lat_diff = dist_func.convertToLat(distance)
+    long_diff = dist_func.convertToLong(distance)
+
+    # print('Diff\nLatitude: ' + str(lat_diff))
+    # print('Longitude: ' + str(long_diff))
+    
+    # Builds rules for distance restrictions
+    lat1 = buildRule('coordinates.latitude', aCoord['latitude']-lat_diff, '>=')
+    lat2 = buildRule('coordinates.latitude', aCoord['latitude']+lat_diff, '<=')
+    long1 = buildRule('coordinates.longitude', aCoord['longitude']-long_diff, '>=')
+    long2 = buildRule('coordinates.longitude', aCoord['longitude']+long_diff, '<=')
+
+    # compiles 4 rules for distance restrictions
+    coordRule = groupRule(andRule(andRule(lat1, lat2), andRule(long1, long2)))
+    
+    return coordRule
 
 # retrieve property of json results from a query
 def specifyItem(data_dict, index=0):
@@ -151,109 +161,3 @@ def printSchedule(dayList):
             print(indexConvert(j) + ' - ' + str(dayList[j]))
         elif (dayList[j] == -3):
             print(indexConvert(j) + ' - The end of your day!')
-
-# Test function
-def runTests(dayList):
-    
-    ## TESTS ##
-    scheduleBounds('17:50', '2:30', dayList)
-    print('####################  [Welcome to DayMaker]  #########################\n')
-    # printSchedule(dayList)
-    # print(indexConvert(50))
-    # print(timeConvert('12:30'))
-
-    # test case variables
-    property_key = 'name'
-    start_time = '20:00'
-    end_time = '23:30'
-
-    # prints the array of all tags
-    print('ALL TAGS: {}\n'.format(getTags()))
-
-    print('What kind of event would you like to schedule?')
-    search_term = input()
-
-    # # limit options by number
-    # print('How many options would you like to choose from?')
-    # results_num = input()
-
-    # creates filter for query ('::' - 'is' operator by default)
-    rule1 = buildRule('review_count', '\"189\"', '::')
-    rule2 = buildRule('price', '\"$\"', '::')
-    rule3 = buildRule('rating', 4.5, '::')
-    group_rule = groupRule(andRule(rule2, rule3))
-    my_filter = orRule(rule1, group_rule)
-
-    ################### ADD LINE COMMENT BELOW TO APPLY FILTER ##########################
-    my_filter = ''
-
-    print('Filtering \"' + search_term + '\" options by ' + my_filter + '\nResults:\n')
-
-    # natural language query and results
-    my_json = natLangQuery(search_term, my_filter)
-    viewResults(my_json)
-
-    # user choice
-    print('Enter a number for the event you would like to schedule:')
-    user_choice = int(input()) - 1
-
-    # user time specification
-    print('Would you like to set a custom time for your event at', specifyItem(my_json, user_choice)[property_key], '? y/n:')
-    user_input = input()
-    if user_input.find('y') != -1:
-        print('When would you like to arrive? Enter in the format of hh:mm')
-        start_time = input()
-        print('When would you like to leave? Enter in the format of hh:mm:')
-        end_time = input()
-
-    # creates event object
-    event_obj = createEvent(specifyItem(my_json, user_choice), start_time, end_time)
-    # schedules event obj
-    scheduleEvent(event_obj, dayList)
-
-    # prints entire schedule
-    # start_index = dayList.index('The start of your day!')
-    # for x in range(start_index, len(dayList)):
-    #     if (dayList[x] != -1):
-    #             print(indexConvert(x) + ' - ' + str(dayList[x]))
-
-    # for x in range(start_index):
-    #     if (dayList[x] != -1):
-    #             print(indexConvert(x) + ' - ' + str(dayList[x]))
-
-    printSchedule(dayList)
-
-    # event_obj.changePlace() method test
-    print('would you like to change the place of your event? id:', event_obj.id, ' y/n:')
-    user_choice = input()
-    if user_choice.find('y') != -1:
-        print('would you like to change the place of this event? select a new place:')
-        viewResults(my_json)
-        print('Enter a number for the place you would like to switch to instead:')
-        user_choice = int(input()) - 1
-        new_place = specifyItem(my_json, user_choice)
-        event_obj.changePlace(new_place)
-
-    # event_obj.changeTime() method test
-    print('would you like to change the time of your event? id: ', event_obj.id, ' y/n:')
-    user_choice = input()
-    if user_choice.find('y') != -1:
-        print('new start time? hh:mm')
-        start_time = input()
-        print('new end time? hh:mm')
-        end_time = input()
-        
-    clearEvent(event_obj, dayList)
-    event_obj.changeTime(start_time, end_time)
-    scheduleEvent(event_obj, dayList)
-
-    # reprints schedule in case changes were made
-    print('Your event starts at ' + event_obj.start)
-    print('Your updated schedule:')
-    printSchedule(dayList)
-
-        
-
-# Calls the main function
-if __name__== "__main__" : 
-    main()
