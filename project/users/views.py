c1ae7bde206ec57c14464157390e1775c782d467
peudy14 <phillipeudy@gmail.com>
2019-11-13 02:51:30 -0500

    Update views.py

--- project/users/views.py
+++ project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 
-from users.forms import CustomUserCreationForm
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
 # Create your views here.
 
 def signup(request):
@@ -13,8 +13,30 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username,password=raw_password)
             login(request,user)
-            return redirect('planner:home')
+            return redirect('planner:index')
 
     else:
         form = CustomUserCreationForm()
     return render(request, 'signup.html',{'form': form})
+def account_details(request):
+    return render(request,'accountDetails.html')
+
+def account_edit(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST,instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('../accountDetails')
+        else:
+            print(form.errors)
+            return render(request,"accountEdit.html",{'form': form})
+
+    else:
+        user = request.user
+        form = CustomUserChangeForm(initial={'username': user.username,'first_name':user.first_name,'last_name': user.last_name,'email': user.email})
+        
+        return render(request, 'accountEdit.html',{'form': form})
+
+            
+            
+    
